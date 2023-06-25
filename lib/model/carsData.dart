108716099de0da1d090fea7f656273f53be24ee9
ip4rsa <ip4rsa@gmail.com>
2023-06-25 17:20:30 +0330

    DataModel-update

--- lib/model/carsData.dart
+++ lib/model/carsData.dart
@@ -1,1 +1,217 @@
+import 'dataModel.dart';
 
+List<CarsModel> carsList = [
+  CarsModel(
+    nameCarFA: "مرسدس اس ال",
+    nameCarEN: "AMG-SL",
+    classCar: "کوپه",
+    imgCar: "assets/img/AMG-SL/silver.png",
+    btnRedmore: 'مشاهده',
+    speed: "320",
+    power: "469 اسب بخار",
+    cityFuelEconomy: "14 mpg",
+    highwayFuelEconomy: "21 mpg",
+    acceleration: "3.8 ثانیه",
+    carVideo:
+        "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my22/sl/class-page/amg/2022-SL-ROADSTER-CT-4-2-DR.mp4",
+    passengerCapacity: "4 نفره",
+    engin: "3982",
+    options: """کالیپر ترمز قرمز
+صندلی های جلو برقی با حافظه
+گرمکن و تهویه صندلی های جلو
+صندلی های جلو چند کانتور فعال با ماساژ
+صندلی های عقب دوتایی
+کنترل آب و هوای اتوماتیک دو منطقه ای
+سیستم گرمایش سطح گردن AIRSCARF
+بادگیر قابل جابجایی
+فرمان گرم کن
+آینه های جانبی تاشو برقی
+تاپ نرم برقی Z-fold
+نور محیطی LED 64 رنگ
+طاقچه های درب نورانی
+نمایشگر نور AMG
+تزئینات داشبورد، در و کنسول با دوخت بالا
+پدال های فولادی ضد زنگ برس خورده
+تجربه کاربری مرسدس بنز (MBUX)
+دسته ابزار دیجیتال 12.3 اینچی
+صفحه نمایش مولتی مدیا مرکزی 11.9 اینچی لمسی قابل تنظیم
+فرمان با دکمه های کنترل لمسی
+فرمان AMG Performance از چرم ناپا
+احراز هویت بیومتریک
+فعال سازی کلمه کلیدی "Hey, Mercedes".
+کنترل صدا با درک زبان طبیعی
+MB ناوبری
+به روز رسانی نقشه ناوبری آنلاین بدون هزینه به مدت 1 سال
+1 سال اطلاعات ترافیک زنده
+به‌روزرسانی‌های نرم‌افزاری از طریق هوا
+اپل کارپلی بی سیم
+بی سیم Android Auto
+سیستم FrontBass
+سیستم صدای فراگیر Burmester
+شارژ بی سیم با جفت شدن NFC
+وای فای داخل ماشین
+پخش آنلاین موسیقی
+اشتراک آزمایشی 6 ماهه پلاتینوم SiriusXM
+گیرنده رادیویی HD
+پخش صدای بلوتوث
+رابط بلوتوث هندزفری
+چندین پورت شارژ USB-C
+بدون کلید-GO
+دسترسی بدون دست
+صندوق عقب الکترونیکی
+سیستم ورودی روشن
+درب بازکن گاراژ
+موتور بیتوربو 4.0 لیتری V8 AMG دست ساز
+گیربکس 9 سرعته AMG SPEEDSHIFT MCT 9G
+AMG Performance 4MATIC+ چهار چرخ متحرک
+AMG DYNAMIC SELECT
+AMG DYNAMICS
+واحد درایو AMG
+سیستم ECO Start/Stop
+آیرودینامیک فعال AIRPANEL
+اسپویلر عقب فعال چند مرحله ای
+فرمان چرخ عقب فعال
+سیستم تعلیق 4 چرخ مولتی لینک
+سیستم تعلیق AMG RIDE CONTROL
+سیستم اگزوز AMG Performance
+سیستم ترمز ترکیبی با کارایی بالا AMG
+کالیپر ترمز قرمز
+ESP Dynamic Cornering Assist
+فرمان برق الکترومکانیکی
+ارتباط Car-to-X
+نوارهای رول پاپ آپ دوتایی
+پیش ایمن
+صدای پیش ایمن
+کمک توجه
+کمک باد متقابل
+دستیار ترمز فعال
+فناوری ترمز تطبیقی
+خدمات تماس اضطراری مرسدس بنز
+کمک نقطه کور با کمک هشدار خروج
+سیستم نمای فراگیر
+PARKTRONIC با دستیار پارک فعال
+چراغ های جلو LED فعال
+دستیار تطبیقی ​​نوربالا
+لامپ های LED در روز و چراغ های عقب
+10 عدد کیسه هوا
+SmartKey با KEYLESS-START
+ردیاب آسیب پارکینگ
+سیستم دزدگیر ضد سرقت
+سیستم ترمز ضد قفل (ABS)
+سیستم کمکی ترمز (BAS)
+برنامه پایداری الکترونیکی (ESP)
+سیستم مانیتورینگ فشار تایر پیشرفته
+برف پاک کن های حساس به باران""",
+    articleCar: "",
+  ),
+  CarsModel(
+    nameCarFA: "جی کلاس",
+    nameCarEN: "G-class",
+    classCar: "شاسی بلند",
+    imgCar: "",
+    btnRedmore: "مشاهده",
+    speed: "240",
+    power: "416 hp",
+    cityFuelEconomy: "13 mpg",
+    highwayFuelEconomy: "16 mpg",
+    acceleration: "5.6 ثانیه",
+    carVideo:
+        "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/g/class-page/series/2023-G-SUV-CT-2-2-DR.mp4",
+    passengerCapacity: "5 نفره",
+    engin: "2925",
+    options: """این ماشین فول اپشن میباشد!""",
+    articleCar: "",
+  ),
+  CarsModel(
+    nameCarFA: "جی تی 43",
+    nameCarEN: "َAMG-GT",
+    classCar: "کوپه",
+    imgCar: "",
+    btnRedmore: "مشاهده",
+    speed: "320",
+    power: "362 اسب بخار",
+    cityFuelEconomy: "19 mpg",
+    highwayFuelEconomy: "25 mpg",
+    acceleration: "3.2 ثانیه",
+    carVideo:
+        "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my22/gt-4dr-coupe/class-page/2022-AMG-GT-4DR-COUPE-CT-3-2-DR.mp4",
+    passengerCapacity: "",
+    engin: "3982",
+    options: """
+صندلی های عقب دوقلوی تاشو 40/40 تقسیم شده
+لیوان های عقب گرم شونده و خنک شونده 
+کنترل تهویه مطبوع اتوماتیک 3 منطقه ای
+Air Balance کابین - سیستم تصفیه هوا
+قابلیت گرمایش سریع برای صندلی های جلو
+دسته های جلو گرم کن و پانل های درب بالایی
+فرمان گرم کن
+دستیار فرمان فعال 
+دستیار فرمان فراری
+دستیار ترمز فعال با عملکرد ترافیک متقابل
+ترمز اضطراری ازدحام
+دستیار توقف اضطراری فعال  Active Emergency Stop Assist
+دستیار محدود سرعت فعال
+کمک نقطه کور فعال
+ Active Lane Keeping Assist
+Active Lane Change Assist  اطلاعات بیشتر در مورد Active Lane Change Assist
+ PRE-SAFE PLUS
+راه‌اندازی مجدد گسترده در ترافیک Stop-and-Go
+سازگاری سرعت مبتنی بر مسیر
+دستیار محدودیت سرعت  
+برنامه موبایل Mercedes me برای آیفون یا اندروید  
+شروع از راه دور  
+قفل / باز کردن درب از راه دور  
+شاخ و چراغ از راه دور  
+وضعیت خودرو از راه دور  
+دانلود POI  
+Geofencing، Valet و Curfew Protect  
+ردیاب خودرو  
+کمک مکان یابی خودروهای سرقتی  
+مکان یاب خودروی پارک شده  
+ارتباط Car-to-X  
+اطلاعات ترافیک زنده  
+پیکربندی خودرو از راه دور  
+ناوبری پیش بینی کننده  
+به روز رسانی آنلاین نقشه  
+پوشش نقشه آب و هوا  
+نقاط دیدنی پارکینگ  
+صفحه نمایش لمسی مرکزی کابین عقب
+""",
+    articleCar: "",
+  ),
+  CarsModel(
+    nameCarFA: "میباخ",
+    nameCarEN: "MERCEDES-MAYBACH",
+    classCar: "واگن",
+    imgCar: "",
+    btnRedmore: "مشاهده",
+    speed: "320",
+    power: "621 hp",
+    cityFuelEconomy: "12 mpg",
+    highwayFuelEconomy: "20 mpg",
+    acceleration: "4.5 ثانیه",
+    carVideo:
+        "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/s/maybach/class-page/series/S_maybach-page-hero-video.mp4",
+    passengerCapacity: "5 نفره",
+    engin: "3982",
+    options: """این ماشین فول اپشن میباشد!""",
+    articleCar: "",
+  ),
+  CarsModel(
+    nameCarFA: "",
+    nameCarEN: "",
+    classCar: "",
+    imgCar: "",
+    btnRedmore: '',
+    speed: "",
+    power: "",
+    cityFuelEconomy: "",
+    highwayFuelEconomy: "",
+    acceleration: "",
+    carVideo: "",
+    passengerCapacity: "",
+    engin: "",
+    options: """""",
+    articleCar: "",
+  ),
+];
--- lib/model/dataModel.dart
+++ lib/model/dataModel.dart
@@ -1,1 +1,35 @@
-class CarsModel {}
+class CarsModel {
+  String nameCarFA;
+  String nameCarEN;
+  String imgCar;
+  String btnRedmore;
+  String classCar;
+  String articleCar;
+  String carVideo;
+  String speed;
+  String acceleration;
+  String power;
+  String options;
+  String passengerCapacity;
+  String cityFuelEconomy;
+  String highwayFuelEconomy;
+  String engin;
+
+  CarsModel({
+    required this.nameCarFA,
+    required this.nameCarEN,
+    required this.imgCar,
+    required this.classCar,
+    required this.btnRedmore,
+    required this.acceleration,
+    required this.articleCar,
+    required this.carVideo,
+    required this.options,
+    required this.power,
+    required this.speed,
+    required this.cityFuelEconomy,
+    required this.highwayFuelEconomy,
+    required this.passengerCapacity,
+    required this.engin,
+  });
+}

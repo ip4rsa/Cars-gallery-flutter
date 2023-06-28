c26da5e9ff6320d2c7290b563a5a4c8b234050cf
ip4rsa <ip4rsa@gmail.com>
2023-06-28 11:11:16 +0330

    homeScreen final (demo)

--- lib/view/homPage.dart
+++ lib/view/homPage.dart
@@ -71,6 +71,7 @@ class _homePageState extends State<homePage> {
           ),
         ),
         body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
           child: Column(
             children: [
               //-----> App Bar
@@ -253,6 +254,84 @@ class _homePageState extends State<homePage> {
                 ],
               ),
 
+              SizedBox(height: 20),
+
+              Container(
+                height: size.height / 3.7,
+                width: size.width / 1.09,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(23)),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment(0.8, 1),
+                    colors: <Color>[
+                      Color.fromRGBO(0, 15, 19, 1),
+                      Color.fromARGB(253, 20, 30, 39),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Icon(
+                          CupertinoIcons.left_chevron,
+                          color: Colors.blueAccent,
+                          size: 30,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                          child: InkWell(
+                            onTap: () {},
+                            child: const Text(
+                              "تاریخچه شرکت مرسدس بنز",
+                              style: TextStyle(
+                                  fontSize: 19,
+                                  fontFamily: 'yekanlight',
+                                  color: Color.fromARGB(255, 255, 255, 255)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                      child: Divider(
+                        color: Color.fromARGB(184, 53, 107, 201),
+                        indent: 22,
+                        endIndent: 22,
+                        thickness: .5,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Icon(
+                          CupertinoIcons.left_chevron,
+                          color: Colors.blueAccent,
+                          size: 30,
+                        ),
+                        SizedBox(width: 10),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                          child: InkWell(
+                            onTap: () {},
+                            child: const Text(
+                              "گالری ماشین ها",
+                              style: TextStyle(
+                                  fontSize: 19,
+                                  fontFamily: 'yekanlight',
+                                  color: Color.fromARGB(255, 255, 255, 255)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
               SizedBox(height: 20),
             ],
           ),

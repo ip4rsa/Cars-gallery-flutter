b39510eaf45d9e492f39dc3dcb465e49dbfcdc11
ip4rsa <ip4rsa@gmail.com>
2023-08-07 23:39:40 +0330

    Update

--- lib/view/SplashScreen.dart
+++ lib/view/SplashScreen.dart
@@ -3,7 +3,6 @@ import 'main-homPage.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
-
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
@@ -11,33 +10,35 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 4)).then((value) {
+    Future.delayed(const Duration(seconds: 10000)).then((value) {
       Navigator.of(context)
           .pushReplacement(MaterialPageRoute(builder: (context) => homePage()));
     });
-
-    super.initState();
   }
 
+  double turns = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Text("کلیک"),
       backgroundColor: const Color.fromARGB(255, 15, 15, 15),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+              child: AnimatedRotation(
+                duration: const Duration(seconds: 25),
+                turns: turns,
                 child: Image.asset(
                   "assets/img/logoMercedes.png",
                   height: 85,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
